5d9d2e833acaa074579928cbad435df74ecb5847
Bradley May <mayb1991@yahoo.com>
2022-07-06 09:04:16 -0600

    first commit

---
+++ in_class.py
@@ -0,0 +1,12 @@
+# year = int(input("What year is your car? "))
+# make = input("what is the make of your car? ")
+# model = input("what is the model? ")
+# color = input("what color is your car? ")
+# car = f"I have a {year} {make} {model} car and it is {color}"
+# print(car)
+# letter = input("What is your name? ")
+# if "p" in letter:
+#     print("theres the P ")
+# else:
+#     print("No P here")
+
